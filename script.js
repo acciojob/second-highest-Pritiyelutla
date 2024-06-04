1b3a749e407d3849aa9d1bc4f37ca9cb04c8b161
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-04 23:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function secondHighest(num, arr) {
+    // your code here
+    if (arr.length < 2) {
+        return "Infinity";
+    }
+
+    arr.sort();
+
+    for(let i=arr.length -2; i>=0; i--)
+    {
+        if(arr[i] !== arr[arr.length - 1])
+        {
+            return arr[i];
+        }
+    }
