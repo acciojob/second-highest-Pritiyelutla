e89912dc9f9258546f7c8c7ec463d88ad7a7d536
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-07 23:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-//your JS code here. If required.
- function secondHighest(arr) {
+function secondHighest(arr) {
     // your code here
      if (arr.length < 2) {
-        return "Infinity";
+        return "-Infinity";
     }
 
-    let uniqueArr = [...new Set(arr)];
+    arr = [...Set(arr)];
     
-    ar = uniqueArr.sort((a, b) =>{
-        if(a < b)
-        {
-            return -1;
-        }
-    });
-res = ar[ar.length-2];
-    
-    if(res !== undefined)
+    let firstMax = null;
+    let temp = null;
+    for(let i=0; i<arr.length - 1; i++)
     {
-        return res;
+        for(let j=0; j<arr.length - i; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        if(firstMax == null)
+            firstMax = arr[arr.length - 1];
+        if(firstMax != arr[arr.length - i - 1]){
+            return arr[arr.length - i - 1];
+        }
     }
-    else{
-        return "Infinity";
-    }
+    return "-Infinity";
  }
